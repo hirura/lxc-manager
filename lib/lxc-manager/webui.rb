ff79b1128f3d41b7ec4c35928c55a1f2cef82c62
hirura <hirura@gmail.com>
2016-08-17 14:18:42 +0900

    update containers view to generate ttl

--- lib/lxc-manager/webui.rb
+++ lib/lxc-manager/webui.rb
@@ -1916,7 +1916,7 @@ class LxcManager
 					config      = lxc_manager.config
 					container   = lxc_manager.containers.find( params[:id] )
 					server_name = request.env['SERVER_NAME']
-					content_type 'ttl'
+					content_type 'text/turtle'
 					attachment "login_#{params[:id]}.ttl"
 					<<-EOB
 					addr = '#{server_name}'
