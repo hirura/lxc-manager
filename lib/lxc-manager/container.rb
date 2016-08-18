585ee829353a8cce028aa6ef77c57b7348e8ec66
hirura <hirura@gmail.com>
2016-08-18 20:51:42 +0900

    not allow same addresses are active on same network in same time

--- lib/lxc-manager/container.rb
+++ lib/lxc-manager/container.rb
@@ -27,5 +27,18 @@ class LxcManager
 		validates :hostname,    presence: true, format: { with: /\A[a-z][a-z0-9-]{,61}[a-z0-9]\z/ }
 		validates :description, presence: true, format: { with: /\A.*\z/ }
 		validates :state,       presence: true, inclusion: { in: [RUNNING, STOPPED, UNKNOWN] }
+
+		validate :state_cannot_be_running_because_same_address_is_already_active
+
+		def state_cannot_be_running_because_same_address_is_already_in_use
+			conflict_interfaces = interfaces.select{ |interface|
+				v4_address = interface.v4_address
+				network = interface.network
+				network.interfaces.find_all_by_v4_address( v4_address ).select{ |all_interface| all_interface.id != interface.id }.any?{ |other_interface| other_interface.container.state == RUNNING }
+			}
+			if conflict_interfaces.any?
+				errors.add( :state, "connot be running because address #{conflict_interfaces.map{ |i| i.v4_address }.join(', ')} is already active in network #{conflict_interfaces.map{ |i| i.network.name }.join(', ')}" )
+			end
+		end
 	end
 end
