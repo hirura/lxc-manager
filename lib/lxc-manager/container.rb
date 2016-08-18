1d02773ed733e51d01a0139f17bd5441e51569ed
hirura <hirura@gmail.com>
2016-08-18 22:10:37 +0900

    fix container

--- lib/lxc-manager/container.rb
+++ lib/lxc-manager/container.rb
@@ -31,15 +31,17 @@ class LxcManager
 		validate :state_cannot_be_running_because_same_address_is_already_active_in_network
 
 		def state_cannot_be_running_because_same_address_is_already_active_in_network
-			conflict_interfaces = interfaces.select{ |interface|
-				v4_address = interface.v4_address
-				network = interface.network
-				same_address_interfaces = network.interfaces.select{ |interface2| interface2.v4_address == v4_address }
-				other_interfaces = same_address_interfaces.select{ |interface2| interface2.id != interface.id }
-				other_interfaces.any?{ |interface2| interface2.container.state == RUNNING }
-			}
-			if conflict_interfaces.any?
-				errors.add( :state, "connot be running because address #{conflict_interfaces.map{ |i| i.v4_address }.join(', ')} is already active in network #{conflict_interfaces.map{ |i| i.network.name }.join(', ')}" )
+			if state == RUNNING
+				conflict_interfaces = interfaces.select{ |interface|
+					v4_address = interface.v4_address
+					network = interface.network
+					same_address_interfaces = network.interfaces.select{ |interface2| interface2.v4_address == v4_address }
+					other_interfaces = same_address_interfaces.select{ |interface2| interface2.id != interface.id }
+					other_interfaces.any?{ |interface2| interface2.container.state == RUNNING }
+				}
+				if conflict_interfaces.any?
+					errors.add( :state, "connot be running because address #{conflict_interfaces.map{ |i| i.v4_address }.join(', ')} is already active in network #{conflict_interfaces.map{ |i| i.network.name }.join(', ')}" )
+				end
 			end
 		end
 	end
