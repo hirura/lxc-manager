57efc31c6c88affd593792e28642a32eb6faee87
hirura <hirura@gmail.com>
2016-09-24 15:26:29 +0900

    fix reverse proxy substitute model

--- lib/lxc-manager/reverse_proxy_substitute.rb
+++ lib/lxc-manager/reverse_proxy_substitute.rb
@@ -11,7 +11,7 @@ class LxcManager
 		belongs_to :reverse_proxy
 
 		validates :reverse_proxy_id, presence: true
-		validates :name,             presence: true, uniqueness: { scope: [:container_id], conditions: -> { where( deleted_at: nil ) } }, format: { with: /\A[a-zA-Z][a-zA-Z0-9 @._-]{,99}\z/ }
+		validates :name,             presence: true, uniqueness: { scope: [:reverse_proxy_id], conditions: -> { where( deleted_at: nil ) } }, format: { with: /\A[a-zA-Z][a-zA-Z0-9 @._-]{,99}\z/ }
 		validates :pattern,          presence: true, format: { with: /\A(?:\\\\|\\'|[^'])*\z/ }
 		validates :replacement,      presence: true, format: { with: /\A(?:\\\\|\\'|[^'])*\z/ }
 	end
