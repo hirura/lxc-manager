210233dfe84664073e793aff1ac182e1a4b8ff21
hirura <hirura@gmail.com>
2016-09-24 13:39:21 +0900

    fix migration again

--- db/migrate/003_update_href_path_on_reverse_proxies.rb
+++ db/migrate/003_update_href_path_on_reverse_proxies.rb
@@ -4,7 +4,7 @@ class UpdateHrefPathOnReverseProxies < ActiveRecord::Migration
 		reverse_proxies.each{ |reverse_proxy|
 			id       = reverse_proxy['id']
 			location = reverse_proxy['location']
-			update( "UPDATE reverse_proxies SET href_path=#{location} WHERE id=#{id}" )
+			update( "UPDATE reverse_proxies SET href_path='#{location}' WHERE id='#{id}'" )
 		}
 	end
 end
