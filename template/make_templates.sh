12ac123eb86f92e716ebf55d9763609df33a39d6
hirura <hirura@gmail.com>
2016-11-12 15:06:37 +0900

    modify templates to support centos/rhel 7.X

--- template/make_templates.sh
+++ template/make_templates.sh
@@ -12,6 +12,18 @@ do
 	chmod +x lxc-${os}
 done
 
+for os in centos70 centos71 centos72
+do
+	cat lxc-centos.in \
+		| sed -E "s/@LXCPATH@/\/usr\/local\/share\/lxc\/config/g" \
+		| sed -E "s/@LXCTEMPLATECONFIG@/\/ext\/lxcpool\/lxc/g" \
+		| sed -E "s/@LOCALSTATEDIR@/\/var/g" \
+		| sed -E "s/__RELEASE__/7/" \
+		| sed -E "s/YUM0=\"yum/YUM0=\"yum -d 1/g" \
+		> lxc-${os}
+	chmod +x lxc-${os}
+done
+
 for os in rhel64 rhel65 rhel66 rhel67 rhel68
 do
 	cat lxc-rhel.in \
@@ -19,6 +31,18 @@ do
 		| sed -E "s/@LXCTEMPLATECONFIG@/\/ext\/lxcpool\/lxc/g" \
 		| sed -E "s/@LOCALSTATEDIR@/\/var/g" \
 		| sed -E "s/__RELEASE__/6/" \
+		| sed -E "s/YUM0=\"yum/YUM0=\"yum -d 1/g" \
+		> lxc-${os}
+	chmod +x lxc-${os}
+done
+
+for os in rhel70 rhel71 rhel72
+do
+	cat lxc-rhel.in \
+		| sed -E "s/@LXCPATH@/\/usr\/local\/share\/lxc\/config/g" \
+		| sed -E "s/@LXCTEMPLATECONFIG@/\/ext\/lxcpool\/lxc/g" \
+		| sed -E "s/@LOCALSTATEDIR@/\/var/g" \
+		| sed -E "s/__RELEASE__/7/" \
 		| sed -E "s/YUM0=\"yum/YUM0=\"yum -d 1/g" \
 		> lxc-${os}
 	chmod +x lxc-${os}
