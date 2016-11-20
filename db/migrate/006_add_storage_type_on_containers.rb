1e4ef9f4d68f8579540c7912f97e130490e7dc20
hirura <hirura@gmail.com>
2016-11-20 14:42:23 +0900

    add db migrations to add storage size on containers table

--- db/migrate/006_add_storage_type_on_containers.rb
+++ db/migrate/006_add_storage_type_on_containers.rb
@@ -1,5 +1,6 @@
 class AddStorageTypeOnContainers < ActiveRecord::Migration
 	def change
 		add_column :containers, :storage_type, :string, null: true
+		add_column :containers, :size_gb, :integer, null: true
 	end
 end
